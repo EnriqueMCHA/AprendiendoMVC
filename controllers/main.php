500f402dbf23a0b4818e045491f479240ede7d5f
Enrique Chacón <enrique.chacon0230@gmail.com>
2022-08-04 16:37:14 -0400

    Primeras configuraciones con los controllers

---
+++ controllers/main.php
@@ -0,0 +1,15 @@
+<?php 
+
+class Main{
+
+    function __construct(){
+        echo '<p>Nuevo controlador main</p>';
+    }
+
+    function saludo(){
+        echo '<p>Estoy en main ejecutando el metodo saludo</p>';
+    }
+
+}
+
+?>
